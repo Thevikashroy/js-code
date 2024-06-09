83f94ae66ba1d4bb4c128e2a0d5265f2d295b9bc
VIKASH KUMAR <114073200+Thevikashroy@users.noreply.github.com>
2024-06-09 05:35:43 +0000

    07_datesinJs

---
+++ 01_basic/07_datesinJs.js
@@ -0,0 +1,36 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString);
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+// let myCreateDate = new Date("2023-01-14")
+let myCreateDate = new Date("01-14-2024")
+// console.log(myCreateDate.toLocaleString());
+
+let mmyTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(MathCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+
+})
+
+
+
+
+
