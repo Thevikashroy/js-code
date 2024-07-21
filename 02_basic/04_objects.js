a906b2a509a783949b5d921bc1df3e022ab69d9a
VIKASH KUMAR <114073200+Thevikashroy@users.noreply.github.com>
2024-07-21 16:17:27 +0000

    jason talk

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -46,11 +46,30 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor:instructor} = course // object destructing the any value
+
+console.log(instructor);
+
+
+[
+    {},
+    {}, // json format
+    {}
+]
