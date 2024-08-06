31717ac848c1381d105a9ab8cf3db75b6d06894f
VIKASH KUMAR <114073200+Thevikashroy@users.noreply.github.com>
2024-08-06 01:59:19 +0000

    one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+// if statement
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if (temperature === 50){
+//     console.log("less than 50");
+// }
+// console.log("Executed");
+
+// console.log("temperature is greater than 50");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     var power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance >= 500) console.log("test");
+
+// if (balance < 500){
+//     console.log("less than ");
+    
+// } else if (balance < 750) {
+//     console.log("less than 750");   
+// } else if(balance < 900) {
+//     console.log("less than 750");
+    
+// } else{
+//     console.log("less than 1200");
+    
+// }
+
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInfromemail = true
+
+if (isUserLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");   
+}
+
+if (loggedInFromGoogle || loggedInfromemail) {
+    console.log("User logged in");
+    
+}
